481d34e80b82af5c124cfccdc8690572a16502a8
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-23 13:57:05 -0400

    first two done

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 function slice(string, start = 0, end = string.length) {
-}
+  let newStr = ''
+  for (let i=start; i<end; i++) {
+    newStr = newStr + string[i]
+  }
+  return newStr
+  }
+
 
 function repeat(str, repetitions) {
+  let newStr = ''
+  for (let i=0; i<repetitions; i++) {
+    newStr =  newStr + str
+  }
+  return newStr
 }
 
 function startsWith(string, substring) {
